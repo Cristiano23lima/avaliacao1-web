37dbcca1300a913996528dc8c48c46097243c47d
Cristiano Rodrigues <devcristiano.rodrigues@gmail.com>
2021-06-14 20:33:03 -0300

    feat: add feat excluir

--- js/main.js
+++ js/main.js
@@ -28,9 +28,59 @@ function mostrarOcultarPopUp(idPopup){
     }
 }
 
+function addMensagem(){
+    let muralMensagens = document.getElementById("mural-mensagens");
+    let nomeInput = document.querySelector("#aprensetacao-nome");
+    let mensagemInput = document.getElementById("apresentacao-mensagem");
+
+    let nome = nomeInput.value;
+    let mensagem = mensagemInput.value;
+
+    if(nome != null && nome != "" && mensagem != null && mensagem != ""){
+        let divAMMs = document.createElement("div");
+        let divAB = document.createElement("div");
+        let divAI = document.createElement("div");
+        let divAA = document.createElement("div");
+        let divAC = document.createElement("div");//div avatar de conteudo
+        let idAleatorio = "mensagem_"+Date.now();
+    
+        divAC.innerHTML = "<p>"+nome+"</p>\
+            <p>"+mensagem+"</p>\
+        ";
+        divAC.setAttribute("class", "apresentacao--conteudo");
+    
+        divAA.innerHTML = nome.substr(0, 2);
+        divAA.setAttribute("class", "apresentacao--avatar");
+    
+        divAB.innerHTML = "<button type='button' onclick='excluirMensagem("+idAleatorio+")'>Excluir</button>";
+        divAB.setAttribute("class", "apresentacao---button");
+    
+        divAI.appendChild(divAA);
+        divAI.appendChild(divAC);
+        divAI.setAttribute("class", "apresentacao--info");
+    
+        divAMMs.appendChild(divAI);
+        divAMMs.appendChild(divAB);
+        divAMMs.setAttribute("class", "apresentacao-mural-mensagem");
+        divAMMs.setAttribute("id", idAleatorio);
+    
+        muralMensagens.appendChild(divAMMs);
+
+        mensagemInput.value=null;
+        nomeInput.value=  null;
+    }else{
+        alert("Por favor, preencha o campo nome e mensagem");
+    }
+
+}
+
+function excluirMensagem(idElemento){
+    let muralMensagens = document.getElementById("mural-mensagens");
+    let filhoASerExcluido = document.getElementById(idElemento);
+    muralMensagens.removeChild(filhoASerExcluido.nodeValue);
+}
+
 var buttonFecharPopUp = document.getElementById("popup-fechar");
-// document.body.onload = mostrarOcultarPopUp('popupApresentacao');
-// document.body.onload = mostrarOcultarPopUp('popup');
 
 document.addEventListener('onload', mostrarOcultarPopUp('popupApresentacao'));
 document.addEventListener('onload', mostrarOcultarPopUp('popup'));
